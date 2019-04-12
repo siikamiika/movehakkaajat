a3d3d9558467bdbb64daf1fdee020ef37194bdbc
Vaintti <antti.jk.vainikka@gmail.com>
2019-04-12 23:12:57 +0300

    Add shooting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float moveSpeed = 1f;
+    float killTimer;
+    float lifeTime = 10f;
+
+    void Start()
+    {
+        killTimer = 0;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed);
+        killTimer += Time.deltaTime;
+        if (killTimer >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    float shootTimer;
+    float shootInterval = 1f;
+    public GameObject bullet;
+    GameObject player;
+
+    void Start()
+    {
+        shootTimer = 0;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if (shootTimer > shootInterval)
+        {
+            shootTimer -= shootInterval;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, player.transform.position, player.transform.rotation);
+    }
+}
