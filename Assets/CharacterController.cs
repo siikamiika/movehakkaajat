32fa1425d962fc6ace1d5c21df7f5441104482a0
Vaintti <antti.jk.vainikka@gmail.com>
2019-04-12 22:00:06 +0300

    Add flying controls and scene to test them

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    Vector2 moveVector;
+
+    void Start()
+    {
+        moveVector = new Vector2(0, 0);
+    }
+
+    void Update()
+    {
+        moveVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveDirection = new Vector3(moveVector.x, 0, moveVector.y);
+        transform.Translate(moveDirection);
+    }
+}
